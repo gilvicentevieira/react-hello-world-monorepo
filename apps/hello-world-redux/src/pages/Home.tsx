8627822016e75bd33c00965c8d55ee52940665de
Gil <gilvicentevieira@gmail.com>
2024-09-10 21:15:23 +0100

    feat(redux): adding Home page

---
+++ apps/hello-world-redux/src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import TypingText from "../components/TypingText"
+
+const Home = () => {
+  return (
+    <section className="px-[10vw] py-20 flex justify-center">
+      Hello World.
+    </section>
+  )
+}
+
+export default Home
