5a7cfabdf1564bfdc36ef1ca7d86a86c9c911ed8
Gil <gilvicentevieira@gmail.com>
2024-09-10 21:08:31 +0100

    adding typingtext comp

---
+++ apps/hello-world-base/src/components/TypingText.tsx
@@ -0,0 +1,15 @@
+
+const TypingText = () => {
+  return (
+    <div className="inline-block p-0">
+      <p className="inline-block overflow-hidden whitespace-nowrap text-primary text-6xl font-extrabold animate-typing">
+        Hello World.
+      </p>
+      {/* <span className="border-r-2 border-[#1A1A1A] animate-blink text-6xl align-top"> */}
+        {/* This is the blinking cursor */}
+      {/* </span> */}
+    </div>
+  );
+};
+
+export default TypingText;
