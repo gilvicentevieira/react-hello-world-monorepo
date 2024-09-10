31162a252e61e45af5f06535a59dc05f7b49aa4f
Gil <gilvicentevieira@gmail.com>
2024-09-10 21:16:37 +0100

    feat(redux): adding mainlayout comp

---
+++ apps/hello-world-redux/src/layouts/MainLayout.tsx
@@ -0,0 +1,19 @@
+import { ReactNode } from "react"
+import Header from "../components/Header";
+
+interface Props {
+  children: ReactNode;
+}
+
+const MainLayout = ({ children }: Props) => {
+  return (
+    <html>
+      <body>
+        <Header/>
+        {children}
+      </body>
+    </html>
+  )
+}
+
+export default MainLayout
