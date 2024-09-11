4b7c4e11ea263a7e0a2daeef4b9d4659766bcdfe
Gil <gilvicentevieira@gmail.com>
2024-09-11 19:16:21 +0100

    feat(router): added Routes config to App.tsx

--- apps/hello-world-router/src/App.tsx
+++ apps/hello-world-router/src/App.tsx
@@ -1,12 +1,23 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
 import MainLayout from './layouts/MainLayout';
 import Home from './pages/Home';
+import About from './pages/About';
+import Contat from './pages/Contact';
+import Contact from './pages/Contact';
+
+const router = createBrowserRouter([
+  { path: '/', element: <MainLayout />, children: [
+      {path: '/', element: <Home/>},
+      {path: '/about', element: <About/>},
+      {path: '/contact', element: <Contact/>},
+    ]
+  }
+])
 
 function App() {
   return (
-    <MainLayout>
-      <Home/>
-    </MainLayout>
+    <RouterProvider router={router} />
   );
 }
 
