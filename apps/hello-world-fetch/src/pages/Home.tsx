0a45bb8a7e348998dd4bb231abc497cd5a2ed6f5
Gil <gilvicentevieira@gmail.com>
2024-09-11 16:20:52 +0100

    feat (fetch): updating home

---
+++ apps/hello-world-fetch/src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import FetchOnce from "../components/FetchOnce"
+import FetchPerSearch from "../components/FetchPerSearch"
+
+
+const Home = () => {
+
+  return (
+    <FetchPerSearch/>
+  )
+}
+
+export default Home
