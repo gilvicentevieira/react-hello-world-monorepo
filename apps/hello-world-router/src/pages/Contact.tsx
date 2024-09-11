26a062e7c6f2b1cf9e2c467b4b810a57808ff746
Gil <gilvicentevieira@gmail.com>
2024-09-11 19:21:00 +0100

    feat(router): contact page

---
+++ apps/hello-world-router/src/pages/Contact.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <section className="px-[10vw] py-20 flex justify-center">
+      <p className="text-6xl text-primary">Contact</p>
+    </section>
+  )
+}
+
+export default Contact
