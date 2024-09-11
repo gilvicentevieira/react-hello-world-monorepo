6d388dcd42161c8faf87837b194459e4e390ea06
Gil <gilvicentevieira@gmail.com>
2024-09-11 19:20:54 +0100

    feat(router): about page

---
+++ apps/hello-world-router/src/pages/About.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className="px-[10vw] py-20 flex justify-center">
+      <p className="text-6xl text-primary">About</p>
+    </section>
+  )
+}
+
+export default About
