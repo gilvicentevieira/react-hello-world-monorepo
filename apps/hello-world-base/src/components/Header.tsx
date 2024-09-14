aa6e84ba2576806a561c7442047149064efd0313
Gil <gilvicentevieira@gmail.com>
2024-09-14 21:26:50 +0100

    feat(base): header improvement

--- apps/hello-world-base/src/components/Header.tsx
+++ apps/hello-world-base/src/components/Header.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
-
 const Header = () => {
   return (
-    <header className='mx-auto flex justify-between items-center px-[10vw] py-2 bg-slate-100'>
-      <p className='text-4xl text-primary'>Live Coding Session</p>
-      <img src='devmojos-logo-header.png' alt='header'/>
+    <header className='h-[10vh] flex justify-between items-center px-4 py-2 lg:px-[10vw] bg-slate-300'>
+      <div className='w-[45%]'>
+        <p className='text-primary text-2xl md:text-3xl lg:text-4xl text-center'>Live Coding Session</p>
+      </div>
+      <div className='w-[45%] flex justify-center'>
+        <img src='devmojos-logo-header.png' alt='header' />
+      </div>
     </header>
   )
 }
