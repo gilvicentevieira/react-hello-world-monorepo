7fad54a93d061f8abb0b5bcdacba20311b722da6
Gil <gilvicentevieira@gmail.com>
2024-09-14 21:23:43 +0100

    feat(storybook): adding improved header - for real

---
+++ apps/hello-world-storybook/src/components/Header.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className='h-[10vh] flex justify-between items-center px-4 py-2 lg:px-[10vw] bg-slate-300'>
+      <div className='w-[45%]'>
+        <p className='text-primary text-2xl md:text-3xl lg:text-4xl text-center'>Live Coding Session</p>
+      </div>
+      <div className='w-[45%] flex justify-center'>
+        <img src='devmojos-logo-header.png' alt='header image' />
+      </div>
+    </header>
+  )
+}
+
+export default Header
