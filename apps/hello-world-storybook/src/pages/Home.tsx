7af9a81d47f5675d467e6406c03738307afd0989
Gil <gilvicentevieira@gmail.com>
2024-09-14 21:23:33 +0100

    feat(storybook): adding improved header

---
+++ apps/hello-world-storybook/src/pages/Home.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <section className="px-[10vw] py-20 flex justify-center text-6xl text-primary text-center">
+      Hello World
+    </section>
+  )
+}
+
+export default Home
