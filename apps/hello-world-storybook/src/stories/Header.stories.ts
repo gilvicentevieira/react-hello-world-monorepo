6274d34bbbf77330803a8dd2cc89a80ab29729b7
Gil <gilvicentevieira@gmail.com>
2024-09-15 23:48:49 +0100

    feat(storybook): added header story

---
+++ apps/hello-world-storybook/src/stories/Header.stories.ts
@@ -0,0 +1,17 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import Header from '../components/Header';
+
+const meta: Meta<typeof Header> = {
+  component: Header,
+};
+
+export default meta;
+type Story = StoryObj<typeof Header>;
+
+export const Primary: Story = {
+  args: {
+    primary: true,
+    label: 'Header',
+    size: 'md'
+  },
+};
