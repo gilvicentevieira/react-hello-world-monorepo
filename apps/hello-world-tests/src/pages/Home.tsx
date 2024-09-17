6402ba975141adab6cc10c38d50697013bf1e553
Gil <gilvicentevieira@gmail.com>
2024-09-17 17:08:58 +0100

    feat(tests): update home page to include fetch comp

--- apps/hello-world-tests/src/pages/Home.tsx
+++ apps/hello-world-tests/src/pages/Home.tsx
@@ -1,9 +1,11 @@
+import FetchPerSearch from "../components/FetchPerSearch"
 
 
 const Home = () => {
   return (
-    <section className="px-[10vw] py-20 flex justify-center">
-      Hello World.
+    <section className="px-[10vw] py-20 flex flex-col justify-center items-center">
+      <p>Hello World.</p>
+      <FetchPerSearch />
     </section>
   )
 }
